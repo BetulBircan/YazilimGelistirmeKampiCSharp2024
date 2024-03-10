befea66e8fc3f583661ca4af3c5333a1be555808
Betül Bircan <betul.bircan_1996@hotmail.com>
2024-03-11 00:34:12 +0300

    integer veri tipi yapıldı

--- 2-C#-Dersleri/CSharpCourse/TypesAndVariables/Program.cs
+++ 2-C#-Dersleri/CSharpCourse/TypesAndVariables/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//Value Types-
+//Integer(int) : Tam Sayıdaki değerleri Tutar.Değer tiplidir.Integerin alabileceği değer -2 milyar 147 milyon 483 bin 648 il3 2 milyar 147 milyon 483 bin 647 değerleri arasındadır.
+
+//veri tipi     //değişken adı   //değer
+int             number1 =        10;
+Console.WriteLine("Number1 is {0}",number1);
+
+int number2 = -2147483648; //hata vermez  | bu iki aınır arasında değer alır.
+int number3 = 2147483647; // hata vermez  |
+
+//Console.WriteLine("Hello, World!");
 Console.ReadLine();
