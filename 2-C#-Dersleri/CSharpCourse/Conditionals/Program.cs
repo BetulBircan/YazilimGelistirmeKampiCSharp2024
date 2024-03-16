a756cfa094daaa7a35d04c2f249424b71a193379
Betül Bircan <betul.bircan_1996@hotmail.com>
2024-03-16 18:46:13 +0300

    switch case bloğu eklendi

--- 2-C#-Dersleri/CSharpCourse/Conditionals/Program.cs
+++ 2-C#-Dersleri/CSharpCourse/Conditionals/Program.cs
@@ -2,13 +2,14 @@
 Console.WriteLine("Hello, World!");
 
 //var number = 10;
-var number = 11;
+//var number = 11;
+var number = 20;
 //mantıksal operatörler, ==
 if (number == 10)
 {
     Console.WriteLine("Number is 10");
 }
-else if(number == 20) 
+else if (number == 20)
 {
     Console.WriteLine("Number is 20");
 }
@@ -18,7 +19,23 @@ else
 }
 
 //Single Line If : İF ELSE bloklaro yerine tek satırda if else yapabiliriz ternary operatörü ile
-                                //şartdoğruysa            //şart yanlışsa
+//şartdoğruysa            //şart yanlışsa
 Console.WriteLine(number == 10 ? "Sayı 10 a eşittir" : "Sayı 10 a eşit değildir.");
+
+//ctrl +k+d : formatlama
+
+//Switch-case bloğu : if else if yerine avantaj olarak kullanılır.
+switch (number)
+{
+    case 10:
+        Console.WriteLine("Sayı 10");
+        break;
+    case 20:
+        Console.WriteLine("Sayı 20");
+        break;
+    default:
+        Console.WriteLine("Sayı 10 veya 20 değildir.");
+        break;
+}
 
 Console.ReadLine();
