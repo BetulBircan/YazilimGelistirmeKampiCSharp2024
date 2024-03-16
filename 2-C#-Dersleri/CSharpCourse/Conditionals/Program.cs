504aeab063ece86816e5ebd582ca270ac45ecb04
Betül Bircan <betul.bircan_1996@hotmail.com>
2024-03-16 18:25:01 +0300

    else if bloğu eklendi

--- 2-C#-Dersleri/CSharpCourse/Conditionals/Program.cs
+++ 2-C#-Dersleri/CSharpCourse/Conditionals/Program.cs
@@ -8,6 +8,10 @@ if (number == 10)
 {
     Console.WriteLine("Number is 10");
 }
+else if(number == 20) 
+{
+    Console.WriteLine("Number is 20");
+}
 else
 {
     Console.WriteLine("Number is not 10");
