76c8fd839697b10e4a67aa13767677b6f13386d6
Betül Bircan <betul.bircan_1996@hotmail.com>
2024-03-19 00:28:57 +0300

    çoklu if yapısı eklendi

--- 2-C#-Dersleri/CSharpCourse/Conditionals/Program.cs
+++ 2-C#-Dersleri/CSharpCourse/Conditionals/Program.cs
@@ -3,39 +3,53 @@ Console.WriteLine("Hello, World!");
 
 //var number = 10;
 //var number = 11;
-var number = 20;
+var number = 201;
 //mantıksal operatörler, ==
-if (number == 10)
-{
-    Console.WriteLine("Number is 10");
-}
-else if (number == 20)
-{
-    Console.WriteLine("Number is 20");
-}
-else
-{
-    Console.WriteLine("Number is not 10");
-}
+//if (number == 10)
+//{
+//    Console.WriteLine("Number is 10");
+//}
+//else if (number == 20)
+//{
+//    Console.WriteLine("Number is 20");
+//}
+//else
+//{
+//    Console.WriteLine("Number is not 10");
+//}
 
 //Single Line If : İF ELSE bloklaro yerine tek satırda if else yapabiliriz ternary operatörü ile
 //şartdoğruysa            //şart yanlışsa
-Console.WriteLine(number == 10 ? "Sayı 10 a eşittir" : "Sayı 10 a eşit değildir.");
+//Console.WriteLine(number == 10 ? "Sayı 10 a eşittir" : "Sayı 10 a eşit değildir.");
 
 //ctrl +k+d : formatlama
 
 //Switch-case bloğu : if else if yerine avantaj olarak kullanılır.
-switch (number)
+//switch (number)
+//{
+//    case 10:
+//        Console.WriteLine("Sayı 10");
+//        break;
+//    case 20:
+//        Console.WriteLine("Sayı 20");
+//        break;
+//    default:
+//        Console.WriteLine("Sayı 10 veya 20 değildir.");
+//        break;
+//}
+
+if(number >= 0 && number <= 100)
 {
-    case 10:
-        Console.WriteLine("Sayı 10");
-        break;
-    case 20:
-        Console.WriteLine("Sayı 20");
-        break;
-    default:
-        Console.WriteLine("Sayı 10 veya 20 değildir.");
-        break;
+    Console.WriteLine("Number is between 0-100");
 }
+else if(number >= 101 && number <= 200)
+{
+    Console.WriteLine("Number is between 101-200");
+}
+else if(number > 200 || number < 0) 
+{
+    Console.WriteLine("Number is less than 0 or greater than 200");
+}
+
 
 Console.ReadLine();
