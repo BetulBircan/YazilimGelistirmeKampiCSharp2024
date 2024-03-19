c0414b9675b680c3bbbcff805a23d30a4831cc56
Betül Bircan <betul.bircan_1996@hotmail.com>
2024-03-20 00:26:08 +0300

    default parametreli method eklendi

--- 2-C#-Dersleri/CSharpCourse/Methods/Program.cs
+++ 2-C#-Dersleri/CSharpCourse/Methods/Program.cs
@@ -6,6 +6,8 @@ Add();
 Add();
 var result = Add2(50,20);
 Console.WriteLine(result);
+var result2 = Add3(4);
+Console.WriteLine(result2);
 Console.ReadLine();
 //void de sadece işlem yap dediğimizde yapar.değer döndürmez
 static void Add()
@@ -20,3 +22,11 @@ static int Add2(int number1, int number2)
     return sonuc;
 }
 
+//default parametreli method . method parametresine varsayılan değer atabiliriz eğer method çağırılırken parametre verilmezse varsayılan değeri atar.
+//varsayılan değer hep methpdun spn parametresine verilir.ilk e verilmez hata verir. ama iki parametre alıyorsa mesela iki parametreye de default değer verilebilir.
+
+static int Add3(int x,int number1 = 30, int number2 = 30)
+{
+    int result = number1 + number2;
+    return result;
+}
