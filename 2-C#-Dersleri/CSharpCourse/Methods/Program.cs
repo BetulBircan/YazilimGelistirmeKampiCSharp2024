042ec86bfe7bd0c8970a5d6b50aaa692714e3f34
Betül Bircan <betul.bircan_1996@hotmail.com>
2024-03-20 00:03:50 +0300

    methodlar için yeni bir proje oluşturuldu

---
+++ 2-C#-Dersleri/CSharpCourse/Methods/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Add();
+Add();
+Add();
+Add();
+Console.ReadLine();
+static void Add()
+{
+    Console.WriteLine("Added!!!!");
+}
