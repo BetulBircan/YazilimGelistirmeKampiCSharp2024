4e53463fc4b02e3ba6e0351532301bb8a4bc8ec0
Betül Bircan <betul.bircan_1996@hotmail.com>
2024-03-20 00:17:08 +0300

    paramtreli method eklendi

--- 2-C#-Dersleri/CSharpCourse/Methods/Program.cs
+++ 2-C#-Dersleri/CSharpCourse/Methods/Program.cs
@@ -4,8 +4,19 @@ Add();
 Add();
 Add();
 Add();
+var result = Add2(50,20);
+Console.WriteLine(result);
 Console.ReadLine();
+//void de sadece işlem yap dediğimizde yapar.değer döndürmez
 static void Add()
 {
     Console.WriteLine("Added!!!!");
 }
+
+//parametreli methodla çalışmak
+static int Add2(int number1, int number2)
+{
+    int sonuc = number1 + number2;
+    return sonuc;
+}
+
